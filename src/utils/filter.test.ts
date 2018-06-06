6b413167270b0e12036b874abec78497d733a7e3
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-06-06 23:45:30 +0200

    utils/filter: Extract `readFromFile()` function

--- src/utils/filter.test.ts
+++ src/utils/filter.test.ts
@@ -1,5 +1,19 @@
 import * as filter from './filter';
 
+describe('readFromFile()', () => {
+  let FIXTURES = [
+    '2017-07-15-lev',
+    'Bayreuth/2018-05-30/Open',
+  ];
+
+  for (let fixture of FIXTURES) {
+    it(fixture, () => {
+      let result = filter.readFromFile(`${__dirname}/../../fixtures/${fixture}/filter.csv`);
+      expect(result).toMatchSnapshot();
+    });
+  }
+});
+
 describe('readHandicapsFromFile()', () => {
   let FIXTURES = [
     '2017-07-15-lev',
--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -2,14 +2,26 @@ import fs = require('fs');
 
 import {csvParse} from 'd3-dsv';
 
-export function readHandicapsFromFile(path: string) {
+export function readFromFile(path: string) {
   let content = fs.readFileSync(path, 'utf8');
-  let lines = csvParse(content);
+  return parse(content);
+}
 
+export function parse(content: string) {
+  return csvParse(content, row => ({
+    ognID: row.ID || '',
+    registration: row.CALL || '',
+    callsign: row.CN || '',
+    type: row.TYPE || '',
+    handicap: row.HANDICAP ? parseInt(row.HANDICAP, 10) : 100,
+  }));
+}
+
+export function readHandicapsFromFile(path: string) {
   let handicaps = Object.create(null);
-  lines.forEach(({ CN, HANDICAP }) => {
-    if (CN) {
-      handicaps[CN] = HANDICAP ? parseInt(HANDICAP, 10) : 100;
+  readFromFile(path).forEach(({ callsign, handicap }) => {
+    if (callsign) {
+      handicaps[callsign] = handicap;
     }
   });
   return handicaps;
