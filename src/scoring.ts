04edcee000fbdff2b0c6a0a21983952940e2cade
Fabian P. Schmidt <kerel@mailbox.org>
2019-06-17 23:38:41 +0200

    AAT: Show min_time_exceeded flag in the ranking

--- src/scoring.ts
+++ src/scoring.ts
@@ -71,7 +71,9 @@ export function createInitialDayResult(result: any, dayFactors: InitialDayFactor
   // Finisher’s Handicapped Speed. (Vh = D / T x Ho / H)
   let Vh = V * (Ho / H) || 0;
 
-  return { completed, _completed, D, _D, H, Dh, T, _T, V, _V, Vh };
+  let aat_min_time_exceeded = result.aat_min_time_exceeded;
+
+  return { aat_min_time_exceeded, completed, _completed, D, _D, H, Dh, T, _T, V, _V, Vh };
 }
 
 export function createIntermediateDayResult(
@@ -110,7 +112,9 @@ export function createIntermediateDayResult(
   // Finisher’s Handicapped Speed. (Vh = D / T x Ho / H)
   let Vh = V * (Ho / H);
 
-  return { completed, _completed, D, _D, H, Dh, T, _T, V, _V, Vh };
+  let aat_min_time_exceeded = result.aat_min_time_exceeded;
+
+  return { aat_min_time_exceeded, completed, _completed, D, _D, H, Dh, T, _T, V, _V, Vh };
 }
 
 export function calculateDayResult(result: InitialDayResult, dayFactors: DayFactors): DayResult {
@@ -196,6 +200,7 @@ export interface DayFactors extends InitialDayFactors {
 }
 
 export interface InitialDayResult {
+  aat_min_time_exceeded: boolean;
   completed: boolean;
   _completed: boolean;
 
